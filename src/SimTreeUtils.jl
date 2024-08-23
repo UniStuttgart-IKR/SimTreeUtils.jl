fe96fa062409f0888d06ca6aad0351da7d5aa88b
fchrstou <filippos.christou@ikr.uni-stuttgart.de>
2024-08-23 09:44:25 +0200

    Added sme DimArray funcionalities

--- src/SimTreeUtils.jl
+++ src/SimTreeUtils.jl
@@ -4,12 +4,14 @@ using Parameters
 using Printf
 using DocStringExtensions
 using Suppressor
+using DimensionalData
 using BSON
 using TOML
 
 export copyresults, findrelpaths, getparameters, simsnum, getsims, getsimspath
 
 include("simulation.jl")
+include("loaddata.jl")
 include("metaanalysis.jl")
 
 end
