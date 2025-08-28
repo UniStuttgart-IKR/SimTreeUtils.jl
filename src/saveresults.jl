38a1522ca60421d0419828919af22774c82d63bb
svnwrts <st172247@stud.uni-stuttgart.de>
2025-08-28 20:21:42 +0200

    vorerst ohne BLOB

--- src/saveresults.jl
+++ src/saveresults.jl
@@ -128,8 +128,8 @@ function normalize(
         push!(rows, new_row)
     else
         new_row = copy(row)
-        new_row["$(name)_BLOB"] = to_blob(data)
-        #new_row["$(name)_DATA"] = string(data)
+        #new_row["$(name)_BLOB"] = to_blob(data)
+        new_row["$(name)_DATA"] = string(data)
 
         push!(rows, new_row)
     end
