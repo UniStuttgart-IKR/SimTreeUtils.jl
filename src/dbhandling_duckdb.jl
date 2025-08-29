fa46609bc8c1e8f9278bd7fa1e71a9955d63bd97
svnwrts <st172247@stud.uni-stuttgart.de>
2025-08-29 15:09:09 +0200

    create table, no append

--- src/dbhandling_duckdb.jl
+++ src/dbhandling_duckdb.jl
@@ -163,15 +163,15 @@ function InsertDuckDBDataFrame(session::SimTreeSession, tableName::String, df::D
     CreateDuckDBTable(session, tableName, columnsDict; schema=schema, defaultColumns=false)
     
     # append data by row
-    appender = DuckDB.Appender(session.duckDBcon, tableName, schema)
-    for i in eachrow(df)
-        for j in i
-            DuckDB.append(appender, j)
-        end
-        DuckDB.end_row(appender)
-    end
+    #appender = DuckDB.Appender(session.duckDBcon, tableName, schema)
+    #for i in eachrow(df)
+    #    for j in i
+    #        DuckDB.append(appender, j)
+    #    end
+    #    DuckDB.end_row(appender)
+    #end
     # close the appender after all rows
-    DuckDB.close(appender)
+    #DuckDB.close(appender)
 end
 function CreateSchema(session::SimTreeSession, schema::Union{String, Nothing}, tableName::String)
     if schema === nothing
